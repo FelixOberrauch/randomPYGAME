d22d22fad054a8eefe6e1cb5d33d2eeed7c7c279
FelixOberrauch <felix.oberrauch@students.htl-ibk.at>
2026-04-20 12:12:51 +0200

    main edited

--- src/main.py
+++ src/main.py
@@ -3,12 +3,30 @@ from submodule import *
 
 # ____SETUP_____
 pg.init()
-HIGHT, WIDTH = 1920 ,1080
-screen = pg.display.set_mode((WIDTH, HIGHT))
+HEIGHT, WIDTH = 1920 ,1080
+screen = pg.display.set_mode((WIDTH, HEIGHT))
 clock = pg.time.Clock()
 
+#___Collors___
+white = (255, 255, 255)
+red = (255, 0, 0)
+black = (0,0,0)
+blue = (0, 0, 255)
+green = (0, 255, 0)
 
 running = True
 while running:
-    pass
     #___EVENTS___
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+    #___RENDER___
+    pg.draw.rect(screen, (255,255,0), pg.Rect(50,50,5,50))
+
+    screen.fill(white)
+
+
+    pg.display.flip()
+
+
+pg.quit()
